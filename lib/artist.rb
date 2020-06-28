a9d5bc9da279a95737475c7faddf66687b7628bd
ndorazil <nathandorazil@live.com>
2020-06-27 22:14:52 -0600

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,26 @@
 class Artist
-    attr_accessor :name
+    attr_accessor :name, :artist
 
     def initialize(name)
         @name = name
+        @songs = []
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
+
+    def add_song_by_name(name)
+        song = Song.new(name)
+        add_song(song)
+    end
+
+    def self.song_count
+        Song.all.count
     end
 end
--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,22 @@
 class Post
-    attr_reader :title
+    attr_accessor :title, :author
 
     @@all = []
 
     def initialize(title)
         @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        if self.author.nil?
+            nil
+        else
+            self.author.name
+        end
     end
 end
